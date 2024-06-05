7e164a4a8b459b84a11b8315e3de4fe221ad6f2d
Pavel <g@gmail.com>
2024-06-05 20:41:11 +0600

    поправил каунтеры

--- services-assignment-start/src/app/services/counter.service.ts
+++ services-assignment-start/src/app/services/counter.service.ts
@@ -3,8 +3,8 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class CounterService{
 
-  ActiveInactiveCounter: number;
-  InactiveActiveCounter: number;
+  ActiveInactiveCounter: number = 0;
+  InactiveActiveCounter: number = 0;
   CountActiveToInactive(){
     console.log('Active to Inactive Counter:' + ++this.ActiveInactiveCounter);
   }
